142d5defd72ee2109dbcb7c6e3ef321ea46e468d
Mattteo1220 <Mcdaddy435@gmail.com>
2017-06-09 01:04:42 -0600

    Began laying down my pseudocode for my Javascript

---
+++ index.js
@@ -0,0 +1,21 @@
+var questions = [
+	//question 1:
+  {
+  	question: "Which branch of government can propose an amendment to the U.S. Constitution?",
+  	answers: [
+  		"Executive",
+  		"Legislative",
+  		"Judicial"
+  	],
+  	correctAnswerString: "Legislative",
+  	correctanswer: 2,
+  	userResponse: null
+  },
+  // question 2:
+  {
+  	question:
+  }
+
+
+
+]
